39a00d57bc95fd766af1d19b91c3f350d5b19cd6
Harrison Heck <harrison.heck@gmail.com>
2016-03-16 16:43:45 -0400

    Added docblocks where needed to stop IDEs from showing void return type.

--- src/Encoder/NoneEncoder.php
+++ src/Encoder/NoneEncoder.php
@@ -5,11 +5,21 @@ namespace Linio\Component\Cache\Encoder;
 
 class NoneEncoder implements EncoderInterface
 {
+    /**
+     * @param $value
+     *
+     * @return string
+     */
     public function encode($value): string
     {
         return $value;
     }
 
+    /**
+     * @param $value
+     *
+     * @return mixed
+     */
     public function decode($value)
     {
         return $value;
